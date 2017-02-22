8b1d1922f75a88a486a23ca7e5c9ced45b54a968
Ramandeep Singh Trehan <ramandeeptrehan@gmail.com>
2017-02-22 13:21:25 +0530

    Use of long long int, range of data types, brute force logic

--- Codechef/Beginner/CatsAndDogs.cpp
+++ Codechef/Beginner/CatsAndDogs.cpp
@@ -14,20 +14,23 @@ int main()
 		//max legs on ground = 4(dogs+cats)
 		//cats-2*dogs: if negative, means all dogs can carry all cats, minLegs=4*dogs
 		//else if positive: then minLegs = (value+dogs)*4
-
+		if(legs%4==0)
+		{
 		long long int maxLegs = 4*(dogs+cats);
 		long long int minLegs;
 		long long int difference = cats - 2*dogs;
-		
+
 		if(difference<=0)
 			minLegs = 4*dogs;
 		else
 			minLegs = 4*(dogs + difference);
 		
-		if(legs>=minLegs && legs<=maxLegs && legs%4==0)
+		if(legs>=minLegs && legs<=maxLegs)
 			cout<<"yes"<<endl;
 		else
 			cout<<"no"<<endl;
 		
+		}
+		else cout<<"no"<<endl;
 	}
 }
