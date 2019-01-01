cad63f878e053550ebb38bc2e20f4b3f44f66eaf
Ramandeep Singh Trehan <ramandeeptrehan@gmail.com>
2019-01-02 00:15:30 +0530

    Create 5. Inverted Half Pyramid (using numbers).cpp

---
+++ Pattern-Problems/5. Inverted Half Pyramid (using numbers).cpp
@@ -0,0 +1,37 @@
+/*
+ Output:
+ 
+ Enter number of rows needed
+ 10
+ 1 2 3 4 5 6 7 8 9 10
+ 1 2 3 4 5 6 7 8 9
+ 1 2 3 4 5 6 7 8
+ 1 2 3 4 5 6 7
+ 1 2 3 4 5 6
+ 1 2 3 4 5
+ 1 2 3 4
+ 1 2 3
+ 1 2
+ 1
+ 
+ */
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    int n;
+    int i, j;
+    
+    cout<<"Enter number of rows needed\n";
+    cin>>n;
+    
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=(n-i+1); j++)
+            cout<<j<<" ";
+        
+        cout<<endl;
+    }
+    return 0;
+}
